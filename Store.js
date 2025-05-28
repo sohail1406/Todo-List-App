164c781e2f3398bec3fa61895953e321c93709fa
Sohail Khan <149872335+sohail1406@users.noreply.github.com>
2025-05-28 15:15:46 +0530

    Delete Store.js

--- Store.js
+++
@@ -1,4 +0,0 @@
-import { createStore } from "redux";
-import rootReducers from "./Reducers/index";
-const store = createStore(rootReducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
-export default store;
